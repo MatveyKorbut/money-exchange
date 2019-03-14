4244508dc64e9a089ec1d23239d49880f71f1c1d
Matvey Korbut <korbutmatvey@gmail.com>
2019-03-14 11:52:28 +0300

    feat: complete

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    let obj={};
+    if (currency > 10000){
+        obj.error = "You are rich, my friend! We don't have so much coins for exchange";
+        return obj;
+    }
+    if (currency>=50){
+        obj.H = Math.floor(currency / 50);
+        currency-= obj.H*50; 
+    }
+    if (currency>=25){
+        obj.Q = Math.floor(currency / 25);
+        currency-= obj.Q*25;
+    }
+    if (currency>=10){
+        obj.D = Math.floor(currency / 10);
+        currency-= obj.D*10;
+    }
+    if (currency>=5){
+        obj.N = Math.floor(currency / 5);
+           currency-= obj.N*5;
+    }  
+    if (currency>=1){
+        obj.P = currency;          
+    }
+    return obj;
 }
